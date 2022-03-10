121ef275851e29178cc10f6279594587ae9782b8
Pablo Fonseca <Pablo.fonsecam@outlook.com>
2022-03-10 00:00:28 -0600

    White container component adapted to heroes list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MaterialModule } from "../material/material.module";
 import { SpinnerComponent } from "./components/spinner/spinner.component";
+import { WhiteContainerComponent } from './components/white-container/white-container.component';
 
 @NgModule({
-    declarations: [SpinnerComponent], 
+    declarations: [SpinnerComponent, WhiteContainerComponent], 
     imports: [MaterialModule, CommonModule], 
-    exports: [SpinnerComponent]
+    exports: [SpinnerComponent, WhiteContainerComponent]
 })
 export class SharedModule {}
