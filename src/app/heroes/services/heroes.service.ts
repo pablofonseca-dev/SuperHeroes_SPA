eba854f78e9930b61f26fbec5b10a807fd3e1b10
Pablo Fonseca <Pablo.fonsecam@outlook.com>
2022-03-11 12:16:13 -0600

    Improve heroes API routes (#5)
    * API routes improved using Environment Variables, global shared typescript file created.

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,26 +2,44 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IHeroe } from '../interfaces/heroes.interfaces';
+import { environment } from 'src/environments/environment';
+import { emptyString } from 'src/app/shared/global';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  private _servicePort: string = "3004";
-  private _serviceURL: string = `http://localhost:${this._servicePort}`
+  private _apiEndpoint: string = environment.apiEndpoint;
 
   constructor(
     private http: HttpClient
   ) { }
 
+  /**
+   * Format an Api URL specifying the 
+   * @param url 
+   * @param params 
+   * @returns 
+   */
+  formatApiRoute = (route: string = emptyString, params?: string): string => {
+    if(route === emptyString) 
+      return emptyString; 
+
+    const parsedUrl = this._apiEndpoint.concat("/" + route).trim();
+    
+    if(!params) 
+      return parsedUrl; 
+
+    return parsedUrl.concat("/" + params);
+  }
 
   /**
    * Query all the information from the registered heroes. 
    * @returns Observable with a collection of IHeroe objects. 
    */
   queryAllHeroes = (): Observable<IHeroe[]> => {
-    return this.http.get<IHeroe[]>(`${this._serviceURL}/heroes`);
+    return this.http.get<IHeroe[]>(this.formatApiRoute("heroes"));
   }
 
   /**
@@ -30,7 +48,7 @@ export class HeroesService {
    * @returns Observable with a IHeroe object. 
    */
   queryHeroeById = (id: string): Observable<IHeroe> => {
-    return this.http.get<IHeroe>(`${this._serviceURL}/heroes/${id}`.trim());
+    return this.http.get<IHeroe>(this.formatApiRoute("heroes", id));
   }
 
 }
